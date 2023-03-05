aaae4d8dff2d5ed4b06d54d4063352f7d46158d0
Weixin <yanweixin@zoho.com>
2023-03-05 13:09:53 +0800

    upgrade to gradle 8.0.2

--- whale-utils/net-utils/build.gradle.kts
+++ whale-utils/net-utils/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 dependencies {
     implementation("io.netty:netty-all")
     implementation("com.squareup.okhttp3:okhttp")
+    runtimeOnly("io.netty", "netty-resolver-dns-native-macos", null, null, "osx-x86_64")
 }
